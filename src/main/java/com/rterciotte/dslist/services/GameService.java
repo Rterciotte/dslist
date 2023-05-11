f2a61226cdac45b7951824a99c35df6fc40b08a4
Rogério Terciotte <rterciotte@gmail.com>
2023-05-11 01:03:18 -0300

    Deploy ready

--- src/main/java/com/rterciotte/dslist/services/GameService.java
+++ src/main/java/com/rterciotte/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.rterciotte.dslist.dto.GameDTO;
 import com.rterciotte.dslist.dto.GameMinDTO;
 import com.rterciotte.dslist.entities.Game;
+import com.rterciotte.dslist.projections.GameMinProjection;
 import com.rterciotte.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)	
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)	
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
