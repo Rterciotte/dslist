e23f7311a5da83ab3c20b90c7d1bc036e8a7c7c4
Rogério Terciotte <rterciotte@gmail.com>
2024-11-12 11:41:18 -0300

    GET game by id

--- src/main/java/com/rterciotte/dslist/services/GameService.java
+++ src/main/java/com/rterciotte/dslist/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rterciotte.dslist.dto.GameDTO;
 import com.rterciotte.dslist.dto.GameMinDTO;
 import com.rterciotte.dslist.entities.Game;
 import com.rterciotte.dslist.repositories.GameRepository;
+
+
 
 @Service
 public class GameService {
@@ -15,8 +19,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
+	
 }
